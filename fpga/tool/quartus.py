5dcfa38fc1bdd9616807758530e6479f77e84d4b
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2019-10-29 09:12:00 -0300

    Fixes on years of copyright into fpga/tool

--- fpga/tool/quartus.py
+++ fpga/tool/quartus.py
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2015-2019 INTI
-# Copyright (C) 2015-2019 Rodrigo A. Melo
+# Copyright (C) 2019 INTI
+# Copyright (C) 2019 Rodrigo A. Melo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
