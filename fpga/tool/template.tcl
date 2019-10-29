a5a35611de3d744c0be8b96e1e60f4da53627d5c
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2019-10-29 08:49:46 -0300

    Small changes in template.tcl

--- fpga/tool/template.tcl
+++ fpga/tool/template.tcl
@@ -1,6 +1,8 @@
 #
-# Copyright (C) 2016-2019 INTI
-# Copyright (C) 2016-2019 Rodrigo A. Melo <rmelo@inti.gob.ar>
+# PyFPGA Master Tcl
+#
+# Copyright (C) 2015-2019 INTI
+# Copyright (C) 2015-2019 Rodrigo A. Melo <rmelo@inti.gob.ar>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,55 +20,61 @@
 # Description: Tcl script to create a new project and performs synthesis,
 # implementation and bitstream generation.
 #
+# Note: fpga_ is used to avoid name collisions.
+#
 
-set tool     <TOOL>
-set project  <PROJECT>
-set strategy <STRATEGY>
-set task     <TASK>
+set tool     @TOOL
+set project  @PROJECT
+set strategy @STRATEGY
+set task     @TASK
 
 proc fpga_create {} {
-<CREATE>
+@CREATE
 }
 
 proc fpga_open {} {
-<OPEN>
-}
-
-proc fpga_files {} {
-<FILES>
+@OPEN
 }
 
 proc fpga_close {} {
-<CLOSE>
+@CLOSE
+}
+
+proc fpga_device {} {
+@DEVICE
+}
+
+proc fpga_files {} {
+@FILES
 }
 
 proc fpga_area_opts {} {
-<AREA_OPTS>
+@AREA_OPTS
 }
 
 proc fpga_power_opts {} {
-<POWER_OPTS>
+@POWER_OPTS
 }
 
 proc fpga_speed_opts {} {
-<SPEED_OPTS>
+@SPEED_OPTS
 }
 
 proc fpga_run_syn {} {
-<SYNTHESIS>
+@SYNTHESIS
 }
 
 proc fpga_run_imp {} {
-<IMPLEMENTATION>
+@IMPLEMENTATION
 }
 
 proc fpga_run_bit {} {
-<BITSTREAM>
+@BITSTREAM
 }
 
 proc fpga_options {} {
     if {[catch {
-<PROJECT_OPTS>
+@PROJECT_OPTS
     } ERRMSG]} {
         puts "ERROR: there was a problem applying your project options.\n"
         puts $ERRMSG
@@ -76,7 +84,7 @@ proc fpga_options {} {
 
 proc fpga_pre_flow {} {
     if {[catch {
-<PRE_FLOW_OPTS>
+@PRE_FLOW_OPTS
     } ERRMSG]} {
         puts "ERROR: there was a problem applying your pre-flow options.\n"
         puts $ERRMSG
@@ -86,7 +94,7 @@ proc fpga_pre_flow {} {
 
 proc fpga_post_syn {} {
     if {[catch {
-<POST_SYN_OPTS>
+@POST_SYN_OPTS
     } ERRMSG]} {
         puts "ERROR: there was a problem applying your post-syn options.\n"
         puts $ERRMSG
@@ -96,7 +104,7 @@ proc fpga_post_syn {} {
 
 proc fpga_post_imp {} {
     if {[catch {
-<POST_IMP_OPTS>
+@POST_IMP_OPTS
     } ERRMSG]} {
         puts "ERROR: there was a problem applying your post-imp options.\n"
         puts $ERRMSG
@@ -106,7 +114,7 @@ proc fpga_post_imp {} {
 
 proc fpga_post_bit {} {
     if {[catch {
-<POST_BIT_OPTS>
+@POST_BIT_OPTS
     } ERRMSG]} {
         puts "ERROR: there was a problem applying your post-bit options.\n"
         puts $ERRMSG
@@ -120,6 +128,7 @@ proc fpga_post_bit {} {
 
 if {[catch {
     fpga_create
+    fpga_device
     fpga_files
     switch $strategy {
         "area"  {fpga_area_opts}
