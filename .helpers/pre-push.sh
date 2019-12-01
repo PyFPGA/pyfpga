e60f7486908622031cf7008bb04753b815db7cc1
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2019-11-30 23:24:55 -0300

    Fixed commit of doc/api-reference.md into pre-push.sh

--- .helpers/pre-push.sh
+++ .helpers/pre-push.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-exec venv/bin/python .helpers/pydoc-md.py fpga.project > doc/api-reference.md
+docfile=doc/api-reference.md
+exec venv/bin/python .helpers/pydoc-md.py fpga.project > $docfile
 
-git add docs/
+git add $docfile
 git commit -m "Updated auto-generated documentation"
