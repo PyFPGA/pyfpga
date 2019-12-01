aa25c28087c15d969bc19fe37665db5f443c19ca
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2019-12-01 00:13:11 -0300

    Moved pre-push doc generation to pre-commit

--- .helpers/pre-commit.sh
+++ .helpers/pre-commit.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Check of code
 exec venv/bin/pycodestyle fpga examples test
 exec venv/bin/pylint fpga
 
 # Check of trailing whitespaces
 exec git diff --check --cached
+
+# Dod generation
+venv/bin/python .helpers/pydoc-md.py fpga.project > doc/api-reference.md
+git add doc
+git commit -m "Updated auto-generated documentation"
--- .helpers/pre-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-venv/bin/python .helpers/pydoc-md.py fpga.project > doc/api-reference.md
-
-git add doc
-git commit -m "Updated auto-generated documentation"
