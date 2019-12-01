e9c9c690317b6edaa2069369d344b66659a6701a
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2019-11-30 23:51:02 -0300

    Fixed pre-push.sh

--- .helpers/pre-push.sh
+++ .helpers/pre-push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-exec venv/bin/python .helpers/pydoc-md.py fpga.project > doc/api-reference.md
+venv/bin/python .helpers/pydoc-md.py fpga.project > doc/api-reference.md
 
-git add docs/
+git add doc
 git commit -m "Updated auto-generated documentation"
