edba38485393dd6ba65f717ebd7df8a7b4b8b695
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-01-04 16:04:21 -0300

    Added 'ram.v' and 'ram.vhdl' under 'hdl' directory

---
+++ examples/hdl/ram.v
@@ -0,0 +1,21 @@
+// A RAM initialized with an external file
+
+module ram (
+    input             clk_i,
+    input             we_i,
+    input       [5:0] addr_i,
+    input      [31:0] data_i,
+    output reg [31:0] data_o
+);
+
+reg [31:0] ram [0:63];
+
+initial $readmemb("memory.dat",ram);
+
+always @(posedge clk_i) begin
+    if (we_i)
+        ram[addr_i] <= data_i;
+    data_o <= ram[addr_i];
+end
+
+endmodule
