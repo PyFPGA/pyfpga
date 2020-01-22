04ca586981bed7c87eeea2e099d89dee964bde9a
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2020-01-22 15:31:41 -0300

    Added HDF file creation for Vivado SDK into design.py
    As proof of concept.

--- examples/vivado/design.py
+++ examples/vivado/design.py
@@ -16,9 +16,18 @@ prj.add_files('../../../core_comblock/src/helpers/fifo_loop.vhdl')
 prj.add_files('../../../core_comblock/src', included=True)
 prj.add_files('../../../core_comblock/examples/test/zybo.tcl', design=True)
 
-prj.add_postbit_opt(
-    'write_hw_platform -fixed -force -include_bit -file design_1_wrapper.xsa'
-)
+prj.add_postbit_opt("""
+    if { [ catch {
+        write_hw_platform -fixed -force -include_bit \
+            -file design_1_wrapper.xsa
+    } ] } {
+        write_hwdef -force -file design_1_wrapper.hwdef
+        write_sysdef -force \
+            -hwdef [glob -nocomplain *.hwdef] \
+            -bitfile [glob -nocomplain *.bit] \
+            -file design_1_wrapper.hdf
+    }
+""")
 
 try:
     prj.generate()
