f78d4fdcb37471e9530391b9c74955f7527f2952
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2020-02-07 18:00:49 -0300

    Small fix and simplification into test/tcl/libero.tcl

--- test/tcl/libero.tcl
+++ test/tcl/libero.tcl
@@ -1,6 +1,6 @@
 new_project -name example -location libero -hdl {VHDL} -family {SmartFusion2}
 
-set_device -family SmartFusion2 -die m2s010 -package tq144 -speed -1
+set_device -family SmartFusion2 -die M2S010 -package tq144 -speed -1
 
 create_links -hdl_source ../../examples/hdl/blinking.vhdl
 create_links -io_pdc ../../examples/libero/mkr.pdc
@@ -9,9 +9,6 @@ build_design_hierarchy
 set_root Blinking
 
 organize_tool_files -tool {PLACEROUTE} -file ../../examples/libero/mkr.pdc -module Blinking -input_type {constraint}
-
-close_project
-open_project libero/example.prjx
 
 run_tool -name {SYNTHESIZE}
 
