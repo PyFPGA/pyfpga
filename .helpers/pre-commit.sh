b95b26f994249a78c045a023e4b66333f64e8584
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2020-02-07 17:13:24 -0300

    Simplified pre-commit and pre-push git hooks using 'set -e'

--- .helpers/pre-commit.sh
+++ .helpers/pre-commit.sh
@@ -1,27 +1,18 @@
 #!/bin/sh
 
-verify() {
-    if [ $? -ne 0 ]; then
-        echo "Fix the errors to proceed the commit."
-        exit 1
-    fi
-}
+set -e
 
 echo "Validating PEP8 compliance ..."
 venv/bin/pycodestyle fpga/ examples/ test/
-verify
 
 echo "Running PyLint ..."
 venv/bin/pylint -j 0 --score no fpga -d R0913
-verify
 
 echo "Updating documentation ..."
 venv/bin/python .helpers/pydoc-md.py fpga.project > doc/api-reference.md
-verify
 git add doc
 
 echo "Checking trailing whitespaces ..."
 git diff --check --cached
-verify
 
 echo "The commit can proceed"
--- .helpers/pre-push.sh
+++ .helpers/pre-push.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-verify() {
-    if [ $? -ne 0 ]; then
-        echo "Fix the errors to proceed the push."
-        exit 1
-    fi
-}
+set -e
 
 for TEST in test/*.py; do
     echo "* Running $TEST"
     venv/bin/python3 $TEST
-    verify
 done
 
 echo "The push can proceed"
