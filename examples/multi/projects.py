ace681c4bfc11a8d65201784475ee0c40cd5d918
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2020-02-07 11:49:09 -0300

    Improved examples/multi/projects.py

--- examples/multi/projects.py
+++ examples/multi/projects.py
@@ -3,8 +3,8 @@
 The main idea of a multi-project is to manage more than one project with the
 same script.
 
-Typically, you would want to change some files and the top level, sometimes
-the part (if different boards are used) and less probable, maybe the tool.
+Typically, you would want to change some files and the top-level, sometimes
+the part (if different boards are used) and, less probable, maybe the tool.
 """
 
 import logging
@@ -14,32 +14,27 @@ from fpga.project import Project
 logging.basicConfig()
 
 PROJECTS = {
-    'example1': {
-        'tool': 'vivado',
-        'part': 'xc7s6cpga196-2',
-        'files': [
-            ['../hdl/blinking.vhdl', 'examples'],
-            ['../hdl/examples_pkg.vhdl', 'examples'],
-            ['../hdl/top.vhdl']
-        ],
-        'top': 'Top'
-    },
-    'example2': {
+    'vivado-prj': {
         'tool': 'vivado',
         'part': 'xc7k70t-3-fbg484',
         'files': [
-            ['../hdl/*.vhdl', 'examples']
+            ('../hdl/blinking.vhdl', 'examples'),
+            ('../hdl/examples_pkg.vhdl', 'examples'),
+            '../hdl/top.vhdl'
         ],
         'top': 'Top'
     },
-    'example3': {
+    'ise-prj': {
+        'tool': 'ise',
+        'part': 'xc6slx9-2-csg324',
+        'files': ['../hdl/blinking.vhdl'],
+        'top': 'Blinking'
+    },
+    'quartus-prj': {
         'tool': 'quartus',
         'part': '5CEBA2F17A7',
-        'files': [
-            ['../hdl/blinking.vhdl', 'examples'],
-            ['../hdl/examples_pkg.vhdl', 'examples'],
-            ['../hdl/top.vhdl', 'examples']
-        ],
+        'includes': ['../hdl/headers1/freq.vh', '../hdl/headers2/secs.vh'],
+        'files': ['../hdl/blinking.v', '../hdl/top.v'],
         'top': 'Top'
     },
 }
@@ -48,12 +43,19 @@ for project in sorted(PROJECTS.keys()):
     PRJ = Project(PROJECTS[project]['tool'], project)
     PRJ.set_outdir('../../build/multi/projects/%s' % project)
     PRJ.set_part(PROJECTS[project]['part'])
+    # Only if there are Verilog Included Files
+    if 'includes' in PROJECTS[project]:
+        for include in PROJECTS[project]['includes']:
+            PRJ.add_include(include)
+    # A VHDL into a PACKAGE were specified as a tuple
     for file in PROJECTS[project]['files']:
-        if len(file) > 1:
+        if type(file) is tuple:
             PRJ.add_files(file[0], file[1])
         else:
-            PRJ.add_files(file[0])
+            PRJ.add_files(file)
+    # Specifying the Top-level
     PRJ.set_top(PROJECTS[project]['top'])
+    # Running implementation
     try:
         PRJ.generate(to_task='imp')
     except Exception as e:
