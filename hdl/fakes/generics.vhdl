71022d2ec0d3651d08caccdcca21fe423635ad5b
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-04-20 19:28:48 -0300

    Improved generics.vhdl and parameters.v under hdl/fakes
    * Added a boolean generic.
    * Added asserts.
    * Added outputs which uses the generics/parameters.

--- hdl/fakes/generics.vhdl
+++ hdl/fakes/generics.vhdl
@@ -1,21 +1,41 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity Params is
    generic (
+      BOO : boolean:=FALSE;
       INT : integer:=0;
-      REA : real:=0.0;
       LOG : std_logic:='0';
       VEC : std_logic_vector(7 downto 0):="00000000";
-      STR : string:="ABCD"
+      STR : string:="ABCD";
+      REA : real:=0.0
    );
    port (
-      d_i :  in std_logic;
-      d_o : out std_logic
+      boo_o : out std_logic;
+      int_o : out std_logic_vector(7 downto 0);
+      log_o : out std_logic;
+      vec_o : out std_logic_vector(7 downto 0);
+      str_o : out std_logic;
+      rea_o : out std_logic
    );
 end entity Params;
 
 architecture RTL of Params is
 begin
-   d_o <= d_i;
+
+   assert BOO=True       report "The boolean is not True" severity failure;
+   assert INT=92         report "The integer is not 92" severity failure;
+   assert LOG='1'        report "The std_logic is not '1'" severity failure;
+   assert VEC="11001100" report "The std_logic_vector is not 11001100" severity failure;
+   assert STR="WXYZ"     report "The string is not WXYZ" severity failure;
+   assert REA=1.0        report "The real is not 1.0" severity failure;
+
+   boo_o <= '1' when BOO else '0';
+   int_o <= std_logic_vector(to_unsigned(INT, 8));
+   log_o <= LOG;
+   vec_o <= VEC;
+   str_o <= '1' when STR="WXYZ" else '0';
+   rea_o <= '1' when REA=1.0 else '0';
+
 end architecture RTL;
--- hdl/fakes/parameters.v
+++ hdl/fakes/parameters.v
@@ -1,14 +1,51 @@
 module Params #(
+   parameter BOO = 0,
    parameter INT = 0,
-   parameter REA = 0.0,
-   parameter LOG = 1'd0,
+   parameter LOG = 1'b0,
    parameter VEC = 8'd0,
-   parameter STR = "ABCD"
+   parameter STR = "ABCD",
+   parameter REA = 0.0
 )(
-   input  wire d_i,
-   output wire d_o
+   output wire boo_o,
+   output wire [7:0] int_o,
+   output wire log_o,
+   output wire [7:0] vec_o,
+   output wire str_o,
+   output wire rea_o
 );
 
-assign d_o = d_i;
+   initial begin
+      if (BOO != 1) begin
+         $display("The boolean is not True");
+         $finish;
+      end
+      if (INT != 92) begin
+         $display("The integer is not 92");
+         $finish;
+      end
+      if (LOG != 1) begin
+         $display("The std_logic is not '1'");
+         $finish;
+      end
+      if (VEC != 8'b11001100) begin
+         $display("The std_logic_vector is not 11001100");
+         $finish;
+      end
+      if (STR != "WXYZ") begin
+         $display("The string is not WXYZ");
+         $finish;
+      end
+      if (REA != 1.0) begin
+         $display("The real is not 1.0");
+         $finish;
+      end
+   end
+
+   assign boo_o = BOO;
+   assign int_o = INT;
+   assign log_o = LOG;
+   assign vec_o = VEC;
+   assign str_o = (STR=="WXYZ") ? 1'b1 : 1'b0;
+   assign rea_o = (REA==1.0) ? 1'b1 : 1'b0;
 
 endmodule
