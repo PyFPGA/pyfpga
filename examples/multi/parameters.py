2cfd3807738087584a5c43644d91a939db1c2978
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-04-19 21:43:59 -0300

    Modified parameters.py to use generics.vhdl and parameters.v
    Some types of generics/parameters, such as the string, are not working.

--- examples/multi/parameters.py
+++ examples/multi/parameters.py
@@ -18,16 +18,17 @@ for hdl in ['vhdl', 'verilog']:
             print("The combination Yosys + VHDL was skipped")
             continue
         PRJ = Project(tool)
-        PRJ.set_param('FREQ', '50000000')
-        PRJ.set_param('SECS', '2')
+        PRJ.set_param('INT', '15')
+        PRJ.set_param('REA', '1.5')
+        PRJ.set_param('LOG', "'1'")
+        PRJ.set_param('VEC', '"10101010"')
+        PRJ.set_param('STR', '"WXYZ"')
         PRJ.set_outdir('../../build/multi/params/%s/%s' % (tool, hdl))
         if hdl == 'vhdl':
-            PRJ.add_files('../../hdl/blinking.vhdl')
+            PRJ.add_files('../../hdl/fakes/generics.vhdl')
         else:
-            PRJ.add_include('../../hdl/headers1/freq.vh')
-            PRJ.add_include('../../hdl/headers2/secs.vh')
-            PRJ.add_files('../../hdl/blinking.v')
-        PRJ.set_top('Blinking')
+            PRJ.add_files('../../hdl/fakes/parameters.v')
+        PRJ.set_top('Params')
         try:
             PRJ.generate(to_task='syn')
         except Exception as e:
