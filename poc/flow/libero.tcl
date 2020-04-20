1ad9ecef9c90ac678537974c130b3e1e3940f4dc
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-04-19 21:53:16 -0300

    Moved poc/*.tcl to poc/flow
    And added a brief description of each sub-directory of poc into README.md.

--- poc/flow/libero.tcl
+++ poc/flow/libero.tcl
@@ -2,13 +2,13 @@ new_project -name example -location libero -hdl {VHDL} -family {SmartFusion2}
 
 set_device -family SmartFusion2 -die M2S010 -package tq144 -speed -1
 
-create_links -hdl_source ../hdl/blinking.vhdl
-create_links -io_pdc ../examples/libero/mkr.pdc
+create_links -hdl_source ../../hdl/blinking.vhdl
+create_links -io_pdc ../../examples/libero/mkr.pdc
 build_design_hierarchy
 
 set_root Blinking
 
-organize_tool_files -tool {PLACEROUTE} -file ../examples/libero/mkr.pdc -module Blinking -input_type {constraint}
+organize_tool_files -tool {PLACEROUTE} -file ../../examples/libero/mkr.pdc -module Blinking -input_type {constraint}
 
 run_tool -name {SYNTHESIZE}
 
