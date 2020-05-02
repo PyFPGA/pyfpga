94eea9d9539f811f66ee954b1fd8ad8ec7b9bf58
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-05-01 21:13:34 -0300

    Small changes into poc/params

--- poc/params/ise.tcl
+++ poc/params/ise.tcl
@@ -7,12 +7,14 @@ project set speed   -2
 
 xfile add ../../hdl/fakes/parameters.v
 project set top Params
-project set "Generics, Parameters" "BOO=True INT=255 LOG=1 VEC=255 STR=\"WXYZ\" REA=1.1" -process "Synthesize - XST"
+# BOO can be 1 or true
+project set "Generics, Parameters" "BOO=true INT=255 LOG=1 VEC=255 STR=\"WXYZ\" REA=1.1" -process "Synthesize - XST"
 process run "Synthesize" -force rerun
 
 xfile add ../../hdl/fakes/generics.vhdl
 project set top Params
-project set "Generics, Parameters" "BOO=True INT=255 LOG='1' VEC=\"11111111\" STR=\"WXYZ\" REA=1.1" -process "Synthesize - XST"
+# BOO can be 1 or true
+project set "Generics, Parameters" "BOO=true INT=255 LOG='1' VEC=\"11111111\" STR=\"WXYZ\" REA=1.1" -process "Synthesize - XST"
 process run "Synthesize" -force rerun
 
 project close
