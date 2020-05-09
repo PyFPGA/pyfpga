27c9578680da99df2f496b7bf3a6db453629ae2d
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-05-09 18:06:40 -0300

    Reverted changes into parameters.py
    Until the implementation of the new set_params features (#35).

--- examples/multi/parameters.py
+++ examples/multi/parameters.py
@@ -18,17 +18,27 @@ for hdl in ['vhdl', 'verilog']:
             print("The combination Yosys + VHDL was skipped")
             continue
         PRJ = Project(tool)
-        PRJ.set_param('INT', '15')
-        PRJ.set_param('REA', '1.5')
-        PRJ.set_param('LOG', "'1'")
-        PRJ.set_param('VEC', '"10101010"')
-        PRJ.set_param('STR', '"WXYZ"')
+        PRJ.set_param('FREQ', '50000000')
+        PRJ.set_param('SECS', '2')
         PRJ.set_outdir('../../build/multi/params/%s/%s' % (tool, hdl))
         if hdl == 'vhdl':
-            PRJ.add_files('../../hdl/fakes/generics.vhdl')
+            PRJ.add_files('../../hdl/blinking.vhdl')
         else:
-            PRJ.add_files('../../hdl/fakes/parameters.v')
-        PRJ.set_top('Params')
+            PRJ.add_include('../../hdl/headers1/freq.vh')
+            PRJ.add_include('../../hdl/headers2/secs.vh')
+            PRJ.add_files('../../hdl/blinking.v')
+        PRJ.set_top('Blinking')
+        # PRJ.set_param('INT', '15')
+        # PRJ.set_param('REA', '1.5')
+        # PRJ.set_param('LOG', "'1'")
+        # PRJ.set_param('VEC', '"10101010"')
+        # PRJ.set_param('STR', '"WXYZ"')
+        # PRJ.set_outdir('../../build/multi/params/%s/%s' % (tool, hdl))
+        # if hdl == 'vhdl':
+        #     PRJ.add_files('../../hdl/fakes/generics.vhdl')
+        # else:
+        #     PRJ.add_files('../../hdl/fakes/parameters.v')
+        # PRJ.set_top('Params')
         try:
             PRJ.generate(to_task='syn')
         except Exception as e:
