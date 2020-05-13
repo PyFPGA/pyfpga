7f3232f83cc383393eccba7cabb344d3dabd3a18
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-05-13 20:05:30 -0300

    Updated ghdl.sh and ghdl-yosys.sh, and added yosys.sh, under poc/flow

--- poc/flow/ghdl-yosys.sh
+++ poc/flow/ghdl-yosys.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-yosys -m ghdl -p 'ghdl -fsynopsys -fexplicit -frelaxed --std=08 ../../hdl/blinking.vhdl -e blinking; synth_xilinx -family xc7; write_edif -pvector bra yosys.edif'
+FLAGS="-fsynopsys -fexplicit -frelaxed --std=08"
+
+ghdl -a $FLAGS --work=examples ../../hdl/blinking.vhdl
+ghdl -a $FLAGS --work=examples ../../hdl/examples_pkg.vhdl
+ghdl -a $FLAGS ../../hdl/top.vhdl
+yosys -m ghdl -p 'ghdl '"$FLAGS"' Top; synth_xilinx -family xc7; write_edif -pvector bra yosys.edif'
--- poc/flow/ghdl.sh
+++ poc/flow/ghdl.sh
@@ -2,5 +2,7 @@
 
 FLAGS="-fsynopsys -fexplicit -frelaxed --std=08"
 
-ghdl -a $FLAGS ../../hdl/blinking.vhdl
-ghdl --synth $FLAGS Blinking
+ghdl -a $FLAGS --work=examples ../../hdl/blinking.vhdl
+ghdl -a $FLAGS --work=examples ../../hdl/examples_pkg.vhdl
+ghdl -a $FLAGS ../../hdl/top.vhdl
+ghdl --synth $FLAGS Top
