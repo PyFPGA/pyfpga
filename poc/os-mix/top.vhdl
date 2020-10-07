02c2b338c747fd3b1642a5c73f60a9b0b8029ddb
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-10-07 10:39:45 -0300

    Added poc/os-mix with attempts of VHDL + Verilog with open-source tools

---
+++ poc/os-mix/top.vhdl
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity Top is
+   port (
+      clk_i :  in std_logic;
+      led_o : out std_logic
+   );
+end entity Top;
+
+architecture Structural of Top is
+   constant FREQ : positive := 50e6;
+begin
+
+   dut: Blinking
+      generic map (FREQ => FREQ, SECS => 1)
+      port map (clk_i => clk_i, led_o => led_o);
+
+end architecture Structural;
