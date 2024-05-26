4d845b679843cfd66df16fa3fe75df794ba3e9f5
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-05-26 13:42:54 -0300

    Modified project to employ enumerations

--- pyfpga/project.py
+++ pyfpga/project.py
@@ -10,110 +10,124 @@ functionalities to create a project, generate a bitstream and
 program a device.
 """
 
+from enum import Enum
 from pathlib import Path
 
 TASKS = ['prj', 'elb', 'syn', 'par', 'bit']
 
-TOOLS = [
-    'ghdl',
-    'ise',
-    'libero',
-    'openflow',
-    'quartus',
-    'vivado',
-    'yosys',
-    'yosys-ise',
-    'yosys-vivado'
-]
+
+class Tool(Enum):
+    """Enumeration of supported FPGA tools."""
+    GHDL = 'ghdl'
+    ISE = 'ise'
+    LIBERO = 'libero'
+    OPENFLOW = 'openflow'
+    QUARTUS = 'quartus'
+    VIVADO = 'vivado'
+    YOSYS = 'yosys'
+    YOSYS_ISE = 'yosys-ise'
+    YOSYS_VIVADO = 'yosys-vivado'
+
+
+class Step(Enum):
+    """Enumeration of supported Steps"""
+    PRJ = 'prj'
+    ELB = 'elb'
+    SYN = 'syn'
+    PAR = 'par'
+    BIT = 'bit'
+
+
+class Hook(Enum):
+    """Enumeration of supported Hooks"""
+    PREFILE = 'prefile'
+    PROJECT = 'project'
+    PREFLOW = 'preflow'
+    POSTSYN = 'postsyn'
+    POSTPAR = 'postpar'
+    POSTBIT = 'postbit'
 
 
 class Project:
     """Class to manage an FPGA project.
 
     :param tool: tool name
-    :type tool: str
+    :type tool: Tool
     :param name: project name (tool name by default)
     :type name: str, optional
     :param data: pre-populated data for the project
     :type data: dict, optional
     :param odir: output directory
     :type odir: str, optional
-    :raises NotImplementedError: unsupported tool
-
-    .. note::
-     Supported tool names are:
-     ``ghdl``
-     ``ise``
-     ``libero``
-     ``openflow``
-     ``quartus``
-     ``vivado``
-     ``yosys``
-     ``yosys-ise``
-     ``yosys-vivado``
+    :raises TypeError: when a value is not a valid enum
+    :raises NotImplementedError: when a method is not implemented yet
     """
 
     def __init__(self, tool, name=None, data=None, odir='results'):
         """Class constructor."""
-        if tool not in TOOLS:
-            raise NotImplementedError(f'unsupported tool ({tool}).')
+        if not isinstance(tool, Tool):
+            raise TypeError('tool must be a Tool enum.')
         self.tool = tool
-        self.name = name or tool
+        self.name = name or tool.value
         self.data = data or {}
         self.odir = Path(odir)
         self.odir.mkdir(parents=True, exist_ok=True)
 
     def set_part(self, name):
-        """Templ placeholder"""
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
     def add_file(self, pathname, filetype=None, library=None, options=None):
-        """Templ placeholder"""
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
     def add_vlog(self, pathname, options=None):
-        """Templ placeholder"""
+        """Temp placeholder"""
         self.add_file(pathname, filetype='vlog', options=options)
 
     def add_slog(self, pathname, options=None):
-        """Templ placeholder"""
+        """Temp placeholder"""
         self.add_file(pathname, filetype='slog', options=options)
 
     def add_vhdl(self, pathname, library=None, options=None):
-        """Templ placeholder"""
+        """Temp placeholder"""
         self.add_file(
             pathname, filetype='vhdl',
             library=library, options=options
         )
 
-    def add_param(self, name, value):
-        """Templ placeholder"""
+    def add_include(self, path):
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
-    def add_include(self, path):
-        """Templ placeholder"""
+    def add_param(self, name, value):
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
     def add_define(self, name, value):
-        """Templ placeholder"""
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
     def set_arch(self, name):
-        """Templ placeholder"""
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
     def set_top(self, name):
-        """Templ placeholder"""
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
 
-    def add_hook(self, stage, hook):
-        """Templ placeholder"""
+    def add_hook(self, hook, content):
+        """Temp placeholder"""
+        # if not isinstance(hook, Hook):
+        #     raise TypeError('hook must be a Hook enum.')
         raise NotImplementedError('Method is not implemented yet.')
 
-    def make(self, last='bit', first='prj', capture=False):
-        """Templ placeholder"""
+    def make(self, end=Step.BIT, start=Step.PRJ, capture=False):
+        """Temp placeholder"""
+        # if not isinstance(end, Step) or not isinstance(start, Step):
+        #     raise TypeError('start and end must be a Step enum.')
         raise NotImplementedError('Method is not implemented yet.')
 
     def prog(self, position=1, bitstream=None):
-        """Templ placeholder"""
+        """Temp placeholder"""
         raise NotImplementedError('Method is not implemented yet.')
