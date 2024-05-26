3d29a99c2f854764074716a54783449da606b673
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-05-26 15:55:30 -0300

    docs: fixed to find pyfpga.project

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import sys, re
+from pathlib import Path
+
+sys.path.insert(0, str(Path.cwd().resolve().parent))
+
 # -- Project information -----------------------------------------------------
 
 project = 'PyFPGA'
@@ -30,12 +35,5 @@ exclude_patterns = ['_build', 'wip']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
