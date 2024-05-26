0717dc143b9d722da00512d49c69220fb7685ec1
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-05-26 15:43:08 -0300

    docs: added intro (empty) and api (automodule)

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+# -*- coding: utf-8 -*-
 
 # -- Project information -----------------------------------------------------
 
@@ -21,30 +6,34 @@ project = 'PyFPGA'
 copyright = '2024, Rodrigo Alejandro Melo'
 author = 'Rodrigo Alejandro Melo'
 
-
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+autodoc_default_options = {
+    "members": True,
+    'undoc-members': True,
+    'inherited-members': True,
+}
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+extlinks = {
+   'repositoy': ('https://github.com/PyFPGA/pyfpga/tree/main/%s', None)
+}
 
+exclude_patterns = ['_build', 'wip']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,14 @@
-.. PyFPGA documentation master file, created by
-   sphinx-quickstart on Sun May 26 13:21:40 2024.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. program:: pyfpga
 
-Welcome to PyFPGA's documentation!
-==================================
+PyFPGA's documentation
+======================
+
+.. image:: _static/logo.png
+   :width: 200 px
+   :align: center
+   :target: https://github.com/PyFPGA/pyfpga
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   intro
+   api
