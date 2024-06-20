2c087d6a74a71252f39a3984801624a586c4d341
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-06-20 17:54:15 -0300

    Finished implementation of _make_prepare for Vivado

--- pyfpga/vivado.py
+++ pyfpga/vivado.py
@@ -8,6 +8,9 @@
 Implements support for Vivado.
 """
 
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-branches
+
 from pyfpga.project import Project
 
 
@@ -21,36 +24,57 @@ class Vivado(Project):
         )
         context = {
             'PROJECT': self.name or 'vivado',
-            'PART': self.data.get('part', 'xc7k160t-3-fbg484'),
-            'TOP': self.data.get('top', 'top')
+            'PART': self.data.get('part', 'xc7k160t-3-fbg484')
         }
         for step in steps:
             context[step] = 1
-        if 'hooks' in self.data:
-            for stage in self.data['hooks']:
-                context[stage.upper()] = '\n'.join(self.data['hooks'][stage])
-        #     if { $LIBRARY != "work" } {
-        #         add_files $FILE
-        #         set_property library $LIBRARY [get_files $FILE]
-        #     } else {
-        #         add_files $FILE
-        if 'arch' in self.data:
-            context['ARCH'] = self.data['arch']
-        if 'defines' in self.data:
-            defines = []
-            for key, value in self.data['defines'].items():
-                defines.append(f'{key}={value}')
-            context['DEFINES'] = ' '.join(defines)
         if 'includes' in self.data:
             includes = []
             for include in self.data['includes']:
                 includes.append(str(include))
             context['INCLUDES'] = ' '.join(includes)
+        files = []
+        if 'files' in self.data:
+            for file in self.data['files']:
+                files.append(f'add_file {file}')
+            for file in self.data['files']:
+                if 'lib' in self.data['files'][file]:
+                    lib = self.data['files'][file]['lib']
+                    files.append(
+                        f'set_property library {lib} [get_files {file}]'
+                    )
+        if 'constraints' in self.data:
+            for file in self.data['constraints']:
+                files.append(f'add_file -fileset constrs_1 {file}')
+            for file in self.data['constraints']:
+                if self.data['constraints'][file] == 'syn':
+                    prop = 'USED_IN_IMPLEMENTATION FALSE'
+                if self.data['constraints'][file] == 'syn':
+                    prop = 'USED_IN_SYNTHESIS FALSE'
+                if self.data['constraints'][file] != 'all':
+                    files.append(f'set_property {prop} [get_files {file}]')
+            first = next(iter(self.data['constraints']))
+            prop = f'TARGET_CONSTRS_FILE {first}'
+            files.append(f'set_property {prop} [current_fileset -constrset]')
+        if files:
+            context['FILES'] = '\n'.join(files)
+        if 'top' in self.data:
+            context['TOP'] = self.data['top']
+        if 'defines' in self.data:
+            defines = []
+            for key, value in self.data['defines'].items():
+                defines.append(f'{key}={value}')
+            context['DEFINES'] = ' '.join(defines)
         if 'params' in self.data:
             params = []
             for key, value in self.data['params'].items():
                 params.append(f'{key}={value}')
             context['PARAMS'] = ' '.join(params)
+        if 'arch' in self.data:
+            context['ARCH'] = self.data['arch']
+        if 'hooks' in self.data:
+            for stage in self.data['hooks']:
+                context[stage.upper()] = '\n'.join(self.data['hooks'][stage])
         self._create_file('vivado', 'tcl', context)
 
     def _prog_prepare(self):
