07e53be5e8b22bd3dabafc6a0f650e9dfe1f78a6
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-06-20 19:28:08 -0300

    Updated Vivado example to use the new PyFPGA

--- examples/vivado/vivado.py
+++ examples/vivado/vivado.py
@@ -1,30 +1,33 @@
-"""Vivado example project."""
+"""Vivado VHDL example project."""
 
 import argparse
-import logging
 
-from fpga.project import Project
-
-logging.basicConfig()
+from pyfpga.vivado import Vivado
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--action', choices=['generate', 'transfer', 'all'], default='generate',
+    '--action', choices=['make', 'prog', 'all'], default='make'
+)
+parser.add_argument(
+    '--source', choices=['vhdl', 'vlog', 'design'], default='vhdl'
 )
 args = parser.parse_args()
 
-prj = Project('vivado')
+prj = Vivado(odir=f'../build/vivado-{args.source}')
 prj.set_part('xc7z010-1-clg400')
 
-prj.set_outdir('../../build/vivado')
-
 prj.add_param('FREQ', '125000000')
-prj.add_files('../../hdl/blinking.vhdl')
-prj.add_files('zybo.xdc')
-prj.set_top('Blinking')
+if args.source == 'vhdl':
+    prj.add_vhdl('../resources/vhdl/blink.vhdl')
+if args.source == 'vlog':
+    prj.add_vlog('../resources/vlog/blink.v')
+prj.add_constraint('../resources/constraints/zybo/timing.xdc', 'syn')
+prj.add_constraint('../resources/constraints/zybo/clk.xdc', 'par')
+prj.add_constraint('../resources/constraints/zybo/led.xdc', 'par')
+prj.set_top('Blink')
 
-if args.action in ['generate', 'all']:
-    prj.generate()
+if args.action in ['make', 'all']:
+    prj.make()
 
-if args.action in ['transfer', 'all']:
-    prj.transfer('fpga')
+if args.action in ['prog', 'all']:
+    prj.prog()
