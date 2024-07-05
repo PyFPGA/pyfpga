154da913850b538f8118e59754458683cfbadec4
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-07-05 20:30:42 -0300

    Add to check basic support based on the Blink module

--- tests/projects/support.py
+++ tests/projects/support.py
@@ -27,55 +27,13 @@ args = parser.parse_args()
 
 print(f'INFO: the Tool Under Test is {args.tool}')
 
-try:
-    print('INFO: checking Verilog Includes')
-    prj = tools[args.tool]()
-    prj.add_vlog('../../examples/sources/vlog/*.v')
-    prj.set_top('Top')
-    prj.add_define('DEFINE1', '1')
-    prj.add_define('DEFINE2', '1')
-    prj.add_param('FREQ', '1')
-    prj.add_param('SECS', '1')
-    prj.make(last='syn')
-    sys.exit('ERROR: something does not work as expected')
-except SystemExit:
-    raise
-except Exception:
-    pass
+print('INFO: checking basic Verilog Support')
+prj = tools[args.tool]()
+prj.add_vlog('../../examples/sources/vlog/blink.v')
+prj.set_top('Blink')
+prj.make(last='syn')
 
-try:
-    print('INFO: checking Verilog Defines')
-    prj = tools[args.tool]()
-    prj.add_vlog('../../examples/sources/vlog/*.v')
-    prj.set_top('Top')
-    prj.add_include('../../examples/sources/vlog/include1')
-    prj.add_include('../../examples/sources/vlog/include2')
-    prj.add_param('FREQ', '1')
-    prj.add_param('SECS', '1')
-    prj.make(last='syn')
-    sys.exit('ERROR: something does not work as expected')
-except SystemExit:
-    raise
-except Exception:
-    pass
-
-try:
-    print('INFO: checking Verilog Parameters')
-    prj = tools[args.tool]()
-    prj.add_vlog('../../examples/sources/vlog/*.v')
-    prj.set_top('Top')
-    prj.add_include('../../examples/sources/vlog/include1')
-    prj.add_include('../../examples/sources/vlog/include2')
-    prj.add_define('DEFINE1', '1')
-    prj.add_define('DEFINE2', '1')
-    prj.make(last='syn')
-    sys.exit('ERROR: something does not work as expected')
-except SystemExit:
-    raise
-except Exception:
-    pass
-
-print('INFO: checking Verilog Support')
+print('INFO: checking advanced Verilog Support')
 prj = tools[args.tool]()
 prj.add_vlog('../../examples/sources/vlog/*.v')
 prj.set_top('Top')
@@ -87,8 +45,62 @@ prj.add_param('FREQ', '1')
 prj.add_param('SECS', '1')
 prj.make(last='syn')
 
+try:
+    print('INFO: checking Verilog Includes Support')
+    prj = tools[args.tool]()
+    prj.add_vlog('../../examples/sources/vlog/*.v')
+    prj.set_top('Top')
+    prj.add_define('DEFINE1', '1')
+    prj.add_define('DEFINE2', '1')
+    prj.add_param('FREQ', '1')
+    prj.add_param('SECS', '1')
+    prj.make(last='syn')
+    sys.exit('ERROR: something does not work as expected')
+except SystemExit:
+    raise
+except Exception:
+    pass
+
+try:
+    print('INFO: checking Verilog Defines Support')
+    prj = tools[args.tool]()
+    prj.add_vlog('../../examples/sources/vlog/*.v')
+    prj.set_top('Top')
+    prj.add_include('../../examples/sources/vlog/include1')
+    prj.add_include('../../examples/sources/vlog/include2')
+    prj.add_param('FREQ', '1')
+    prj.add_param('SECS', '1')
+    prj.make(last='syn')
+    sys.exit('ERROR: something does not work as expected')
+except SystemExit:
+    raise
+except Exception:
+    pass
+
+try:
+    print('INFO: checking Verilog Parameters Support')
+    prj = tools[args.tool]()
+    prj.add_vlog('../../examples/sources/vlog/*.v')
+    prj.set_top('Top')
+    prj.add_include('../../examples/sources/vlog/include1')
+    prj.add_include('../../examples/sources/vlog/include2')
+    prj.add_define('DEFINE1', '1')
+    prj.add_define('DEFINE2', '1')
+    prj.make(last='syn')
+    sys.exit('ERROR: something does not work as expected')
+except SystemExit:
+    raise
+except Exception:
+    pass
+
 if args.tool not in ['ise', 'openflow']:
-    print('INFO: checking System Verilog Support')
+    print('INFO: checking basic System Verilog Support')
+    prj = tools[args.tool]()
+    prj.add_slog('../../examples/sources/slog/blink.sv')
+    prj.set_top('Blink')
+    prj.make(last='syn')
+
+    print('INFO: checking advanced System Verilog Support')
     prj = tools[args.tool]()
     prj.add_slog('../../examples/sources/slog/*.sv')
     prj.set_top('Top')
@@ -101,6 +113,21 @@ if args.tool not in ['ise', 'openflow']:
     prj.make(last='syn')
 
 if args.tool not in ['openflow']:
+    print('* INFO: checking basic VHDL Support')
+    prj = tools[args.tool]()
+    prj.add_vhdl('../../examples/sources/vhdl/blink.vhdl')
+    prj.set_top('Blink')
+    prj.make(last='syn')
+
+    print('* INFO: checking advanced VHDL Support')
+    prj = tools[args.tool]()
+    prj.add_vhdl('../../examples/sources/vhdl/*.vhdl', 'blink_lib')
+    prj.add_vhdl('../../examples/sources/vhdl/top.vhdl')
+    prj.set_top('Top')
+    prj.add_param('FREQ', '1')
+    prj.add_param('SECS', '1')
+    prj.make(last='syn')
+
     try:
         print('INFO: checking VHDL Generics')
         prj = tools[args.tool]()
@@ -114,13 +141,4 @@ if args.tool not in ['openflow']:
     except Exception:
         pass
 
-    print('* VHDL Support')
-    prj = tools[args.tool]()
-    prj.add_vhdl('../../examples/sources/vhdl/*.vhdl', 'blink_lib')
-    prj.add_vhdl('../../examples/sources/vhdl/top.vhdl')
-    prj.set_top('Top')
-    prj.add_param('FREQ', '1')
-    prj.add_param('SECS', '1')
-    prj.make(last='syn')
-
-print(f'INFO: Class Under Test works as expected')
+print(f'INFO: Tool Under Test works as expected')
