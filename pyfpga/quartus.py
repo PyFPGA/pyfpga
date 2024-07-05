7963eb1c36bd518b50ec9b175b5e63d114246bff
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-07-05 20:34:09 -0300

    Quartus support improved/simplified

--- pyfpga/quartus.py
+++ pyfpga/quartus.py
@@ -25,50 +25,21 @@ class Quartus(Project):
         }
         for step in steps:
             context[step] = 1
-        if 'includes' in self.data:
-            includes = []
-            for include in self.data['includes']:
-                includes.append(str(include))
-            context['INCLUDES'] = ' '.join(includes)
-        files = []
-        if 'files' in self.data:
-            types = {
-                'slog': 'SYSTEMVERILOG_FILE',
-                'vhdl': 'VHDL_FILE',
-                'vlog': 'VERILOG_FILE'
-            }
-            for file in self.data['files']:
-                hdl = self.data['files'][file]['hdl']
-                lib = self.data['files'][file].get('lib', None)
-                typ = types[hdl]
-                line = f'set_global_assignment -name {typ} {file}'
-                if lib:
-                    line += f' -library {lib}'
-                files.append(line)
-        if 'constraints' in self.data:
-            for file in self.data['constraints']:
-                if file.suffix == '.sdc':
-                    line = f'set_global_assignment -name SDC_FILE {file}'
-                else:
-                    line = f'source {file}'
-                files.append(line)
-        if files:
-            context['FILES'] = '\n'.join(files)
-        if 'top' in self.data:
-            context['TOP'] = self.data['top']
-        if 'defines' in self.data:
-            defines = []
-            for key, value in self.data['defines'].items():
-                defines.append(f'{key} {value}')
-            context['DEFINES'] = ' '.join(defines)
-        if 'params' in self.data:
-            params = []
-            for key, value in self.data['params'].items():
-                params.append(f'{key} {value}')
-            context['PARAMS'] = ' '.join(params)
+        context['INCLUDES'] = self.data.get('includes', None)
+        context['FILES'] = self.data.get('files', None)
+        context['CONSTRAINTS'] = self.data.get('constraints', None)
+        context['TOP'] = self.data.get('top', None)
+        context['DEFINES'] = self.data.get('defines', None)
+        context['PARAMS'] = self.data.get('params', None)
         if 'hooks' in self.data:
-            for stage in self.data['hooks']:
-                context[stage.upper()] = '\n'.join(self.data['hooks'][stage])
+            context['PRECFG'] = self.data['hooks'].get('precfg', None)
+            context['POSTCFG'] = self.data['hooks'].get('postcfg', None)
+            context['PRESYN'] = self.data['hooks'].get('presyn', None)
+            context['POSTSYN'] = self.data['hooks'].get('postsyn', None)
+            context['PREPAR'] = self.data['hooks'].get('prepar', None)
+            context['POSTPAR'] = self.data['hooks'].get('postpar', None)
+            context['PRESBIT'] = self.data['hooks'].get('prebit', None)
+            context['POSTBIT'] = self.data['hooks'].get('postbit', None)
         self._create_file('quartus', 'tcl', context)
         return 'quartus_sh --script quartus.tcl'
 
