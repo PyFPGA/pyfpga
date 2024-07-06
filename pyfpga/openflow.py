8d2a067f4bd8374b6e7e7fda54dac8cd866eb0d8
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-07-06 13:42:33 -0300

    Openflow support improved/simplified

--- pyfpga/openflow.py
+++ pyfpga/openflow.py
@@ -28,49 +28,21 @@ class Openflow(Project):
         }
         for step in steps:
             context[step] = 1
-        if 'includes' in self.data:
-            includes = []
-            for include in self.data['includes']:
-                includes.append(f'-I{str(include)}')
-            context['INCLUDES'] = ' '.join(includes)
-        files = []
-        if 'files' in self.data:
-            for file in self.data['files']:
-                files.append(f'read_verilog -defer {file}')
-        if files:
-            context['VLOGS'] = '\n'.join(files)
-#         for file in self.files['vhdl']:
-#             lib = ''
-#             if file[1] is not None:
-#                 lib = f'--work={file[1]}'
-#             vhdls.append(f'{self.tools["ghdl"]} -a $FLAGS {lib} {file[0]}')
-#         for file in self.files['verilog']:
-#             if file[0].endswith('.sv'):
-#                 verilogs.append(f'read_verilog -sv -defer {file[0]}')
-#             else:
-#                 verilogs.append(f'read_verilog -defer {file[0]}')
-#         if len(vhdls) > 0:
-#             verilogs = [f'ghdl $FLAGS {self.top}']
-        if 'constraints' in self.data:
-            constraints = []
-            for constraint in self.data['constraints']:
-                constraints.append(str(constraint))
-            context['CONSTRAINTS'] = ' '.join(constraints)
-        if 'top' in self.data:
-            context['TOP'] = self.data['top']
-        if 'defines' in self.data:
-            defines = []
-            for key, value in self.data['defines'].items():
-                defines.append(f'-D{key}={value}')
-            context['DEFINES'] = ' '.join(defines)
-        if 'params' in self.data:
-            params = []
-            for key, value in self.data['params'].items():
-                params.append(f'-set {key} {value}')
-            context['PARAMS'] = ' '.join(params)
+        context['INCLUDES'] = self.data.get('includes', None)
+        context['FILES'] = self.data.get('files', None)
+        context['CONSTRAINTS'] = self.data.get('constraints', None)
+        context['TOP'] = self.data.get('top', None)
+        context['DEFINES'] = self.data.get('defines', None)
+        context['PARAMS'] = self.data.get('params', None)
         if 'hooks' in self.data:
-            for stage in self.data['hooks']:
-                context[stage.upper()] = '\n'.join(self.data['hooks'][stage])
+            context['PRECFG'] = self.data['hooks'].get('precfg', None)
+            context['POSTCFG'] = self.data['hooks'].get('postcfg', None)
+            context['PRESYN'] = self.data['hooks'].get('presyn', None)
+            context['POSTSYN'] = self.data['hooks'].get('postsyn', None)
+            context['PREPAR'] = self.data['hooks'].get('prepar', None)
+            context['POSTPAR'] = self.data['hooks'].get('postpar', None)
+            context['PRESBIT'] = self.data['hooks'].get('prebit', None)
+            context['POSTBIT'] = self.data['hooks'].get('postbit', None)
         self._create_file('openflow', 'sh', context)
         return 'bash openflow.sh'
 
